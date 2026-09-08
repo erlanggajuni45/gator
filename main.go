57490fd325c211a98a59306ba7a2490bec2ed589
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-09-08 09:29:48 +0700

    read config

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"gator/internal/config"
+	"log"
+	"os"
+)
 
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	err = cfg.SetUser("erlangga")
+	if err != nil {
+		log.Fatalf("error setting user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Println(cfg.DBURL)
+	os.Exit(0)
 }
