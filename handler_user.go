819be5e8cedc6e2a31c028b76ffe02616f1d6b32
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-09-08 10:52:42 +0700

    register handler now store to db

--- handler_user.go
+++ handler_user.go
@@ -17,3 +17,29 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Printf("User '%s' has been set.\n", username)
 	return nil
 }
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("username is required")
+	}
+
+	username := cmd.Args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      username,
+	})
+
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		return fmt.Errorf("failed to register user: %v", err)
+	}
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("failed to set user: %v", err)
+	}
+
+	fmt.Printf("User '%s' has been registered and set as the current user.\n", user.Name)
+	return nil
+}
