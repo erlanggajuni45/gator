8acc498f3f2c7e7c3fdede56c053e8e2eaf0cf91
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-09-09 14:10:54 +0700

    Create 003_feed_fellows.sql

---
+++ sql/schema/003_feed_fellows.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE feed_fellows (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_id UUID NOT NULL,
+  feed_id UUID NOT NULL,
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT fk_feed FOREIGN KEY (feed_id) REFERENCES feeds(id),
+  CONSTRAINT unique_user_feed UNIQUE (user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_fellows;
