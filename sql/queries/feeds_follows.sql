9de8015d385abbee4812451da327d1a61ee5b1bb
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-09-09 14:42:45 +0700

    query for get feed that followed by given user

--- sql/queries/feeds_follows.sql
+++ sql/queries/feeds_follows.sql
@@ -8,3 +8,9 @@ SELECT inserted.*, feeds.name as feed_name, users.name as user_name
   FROM inserted
   JOIN feeds ON inserted.feed_id = feeds.id
   JOIN users ON inserted.user_id = users.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, feeds.name as feed_name
+FROM feed_follows
+JOIN feeds ON feed_follows.feed_id = feeds.id
+WHERE feed_follows.user_id = $1;
